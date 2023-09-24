316275e86f892511c557586714bbbc2d6579b2c8
unknown <mayank.kumar@ebix.com>
2023-09-24 20:03:52 +0530

    folder structure server

--- server/app/index.ts
+++ server/app/index.ts
@@ -5,12 +5,20 @@ import express from 'express';
 const app = express();
 
 dotenv.config()
+let port:Number
+interface connection{
+  user: String,
+  host: String,
+  database: String,
+  password: String,
+  port: Number
+}
 const client = new Client({
   user: process.env.USERNAME,
   host: process.env.HOST,
   database: process.env.DATABSE,
   password: process.env.PASSWORD,
-  port: 5432,
+  port: 5432
 })
 
 client.connect
